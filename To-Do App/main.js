d48d61ad93f825fff5a55b6b236dee19668c5da6
jamnny <164428308+jamnny@users.noreply.github.com>
2024-08-21 15:56:17 +0800

    Update main.js
    Updated with sweetalert

--- To-Do App/main.js
+++ To-Do App/main.js
@@ -68,7 +68,13 @@ let deleteTask = (e) => {
   e.parentElement.parentElement.remove();
   data.splice(e.parentElement.parentElement.id, 1);
   localStorage.setItem("data", JSON.stringify(data));
-  console.log(data);
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Your work has been deleted",
+    showConfirmButton: false,
+    timer: 1500
+  });
   
 };
 
